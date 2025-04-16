c9dff5b84bdabb5a3909809ad43e852a540d6442
youngkevinw <youngkevinw8@hotmail.com>
2025-04-15 17:14:39 -0700

    added jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [
+      '**/tests/**/*.test.ts',
+    ],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1'
+    }
+  };
