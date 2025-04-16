9cc8ea2fe702c58c132e046afff949665fac92ed
youngkevinw <youngkevinw8@hotmail.com>
2025-04-15 17:18:35 -0700

    added jest

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,17 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: [
       '**/tests/**/*.test.ts',
+      '**/*.spec.ts',
     ],
     moduleNameMapper: {
       '^@/(.*)$': '<rootDir>/src/$1'
+    },
+
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text-summary', 'lcov'],
+  
+    transform: {
+      '^.+\\.ts$': 'ts-jest'
     }
   };
